4f2ca5e60bef201a707d6e471cb48c608bdb70f1
zakkiislamii <mzislami05@gmail.com>
2024-04-26 21:00:12 +0700

    5

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"zakki-store/models"
 	"zakki-store/routers"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load("config/.env")
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		os.Exit(1)
+	}
 	models.ConnectDB()
-	routers.StartServer().Run(":" + os.Getenv("PGPORT"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "22301" // Default to port 8080 if PORT environment variable is not set
+	}
+	routers.StartServer().Run(":" + port)
 }
